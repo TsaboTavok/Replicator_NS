8dcd42c1e11ff75c7d111a3ebe80cddaa7d2ed7a
Nikita Snitnikov <snitnik@gmail.com>
2014-10-10 22:28:17 +0400

    Файл которого наверняка не хватает в предыдущих комитах =)

---
+++ ReplicatorService/ReplicationServiceManagers/ReplicationServiceManagerBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceModel;
+using System.ServiceModel.Configuration;
+using System.Threading.Tasks;
+using ReplicatorService.Callback;
+
+namespace ReplicatorService.ReplicationServiceManagers
+{
+    public abstract class ReplicationServiceManagerBase : IReplicationServiceManager
+    {
+        protected IReplicatorServiceCallback ReplicatorServiceCallback;
+        private Guid _managerGuid;
+
+        public void Init(IReplicatorServiceCallback replicatorServiceCallback)
+        {
+            _managerGuid = Guid.NewGuid();
+
+            var echoAwareCallback = new EchoAwareReplicatorCallback(replicatorServiceCallback, _managerGuid);
+            ReplicatorServiceCallback = echoAwareCallback;
+            InitInternal();
+        }
+
+        protected virtual void InitInternal()
+        {
+        }
+
+        public void SendUpdates(ReplicatorDto replicatorDto)
+        {
+            replicatorDto.CallerGuid = _managerGuid;
+            SendUpdatesInternal(replicatorDto);
+        }
+
+        protected abstract void SendUpdatesInternal(ReplicatorDto replicatorDto);
+
+        protected ServiceModelSectionGroup GetServiceModelSectionGroup()
+        {
+            var group =
+                ServiceModelSectionGroup.GetSectionGroup(
+                    ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None));
+            return @group;
+        }
+        
+        public void Dispose()
+        {
+            DisposeInternal();
+            GC.SuppressFinalize(this);
+        }
+
+        public abstract void DisposeInternal();
+
+        ~ReplicationServiceManagerBase()
+        {
+            this.Dispose();
+        }
+    }
+}
